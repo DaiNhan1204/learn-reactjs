482bdac40a6a92dcd7448b27595432c0081ee506
Tran Dai Nhan <trandainhan125412@gmail.com>
2023-11-24 01:51:34 +0700

    props

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Nhan'
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nhan Dep Trai
+          Nhan
         </p>
+
+        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+
+        {isMale && (
+          <div>
+            <p>Male</p>
+            <p>Male</p>
+            <p>Male</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male</p>
+            <p>Male</p>
+            <p>Male</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male</p>
+            <p>Male</p>
+            <p>Male</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map(color => (
+            <li key={color} style={{ color }}>{color}</li>
+          ))}
+        </ul>
+
       </header>
     </div>
   );
