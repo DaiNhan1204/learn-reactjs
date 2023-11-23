d24bc774301c0d087e0d707d8850417826b4564e
Tran Dai Nhan <trandainhan125412@gmail.com>
2023-11-23 21:28:16 +0700

    Update Tran Dai Nhan

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dai Nhan
+          Tran Dai Nhan
         </p>
       </header>
     </div>
