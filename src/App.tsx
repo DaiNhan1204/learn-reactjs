a60e57e8cd718a808772938603162893fe813d41
Tran Dai Nhan <trandainhan125412@gmail.com>
2023-11-25 03:00:34 +0700

    uodate

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import AlbumFeature from './features/Album';
 function App() {
-  const name = 'Nhan'
-  const age = 18;
-  const isMale = true;
-  const student = {
-    name: 'Easy Frontend'
-  };
-  const colorList = ['red', 'green', 'blue'];
-
+  // const personName = {
+  //   first: 'Dai Nhan',
+  //   last: 'Tran'
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nhan
-        </p>
-
-        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
-
-        {isMale ? <p>Male</p> : <p>Female</p>}
-
-        {isMale && <p>Male</p>}
-        {!isMale && <p>Female</p>}
-
-
-        {isMale && (
-          <div>
-            <p>Male</p>
-            <p>Male</p>
-            <p>Male</p>
-          </div>
-        )}
-
-        {isMale && (
-          <React.Fragment>
-            <p>Male</p>
-            <p>Male</p>
-            <p>Male</p>
-          </React.Fragment>
-        )}
-
-        {isMale && (
-          <>
-            <p>Male</p>
-            <p>Male</p>
-            <p>Male</p>
-          </>
-        )}
-
-        <p>{student.name}</p>
-
-        <ul>
-          {colorList.map(color => (
-            <li key={color} style={{ color }}>{color}</li>
-          ))}
-        </ul>
-
-      </header>
+      <AlbumFeature />
+      {/* <TodoFeature /> */}
+      {/* <Greet name='Nhan' messageCount={15} /> */}
+      {/* <Person name={personName} /> */}
     </div>
   );
 }
